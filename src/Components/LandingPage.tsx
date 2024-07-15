380dfc443d3175b8e40096007176fec4ea4fab45
Matt Enterline <matthew.j.enterline@gmail.com>
2024-07-15 14:07:43 -0400

    Calling spotify login API and getting user profile

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,9 +1,19 @@
 import { Button } from "@headlessui/react";
+import { fetchProfile, getAccessToken, Login } from "./Login/utils";
+import { useEffect, useState } from "react";
+import { UserProfile } from "../types/UserProfile";
 
 export default function LandingPage() {
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get("code");
+  const clientId = "13f6cc16e88b452db78769d2ca4487b3";
+  const [userProfile, setUserProfile] = useState<UserProfile | undefined>();
+  if (code && !userProfile) Login(code ?? undefined, setUserProfile);
+
+  console.log("profile = ", userProfile);
   return (
     <div className="flex flex-col gap-16">
-      <h1 className="text-4xl text-spotifyGreen">SPOTIFY - STATS</h1>
+      <header className="text-4xl text-spotifyGreen">SPOTIFY - STATS</header>
       <section className="flex flex-col gap-4">
         <h3 className="text-xl text-spotifyGreen">
           What if you could see your spotify wrapped at any time?
@@ -13,15 +23,23 @@ export default function LandingPage() {
           track you listened to on repeat?
         </h3>
       </section>
-      <div className="flex flex-col items-center gap-4">
-        <Button className="rounded-full self-center bg-spotifyGreen text-spotifyBlack data-[hover]:bg-spotifyBlack data-[hover]:text-spotifyGreen transition ease-in-out duration-500 px-4 py-2">
-          Login to Spotify
-        </Button>
-        <p className="text-spotifyText max-w-[512px]">
-          Please login to spotify so in order to fetch your listening data.
-          Nothing is saved or persisted in any way
-        </p>
-      </div>
+      {!code && (
+        <div className="flex flex-col items-center gap-4">
+          <Button
+            className="rounded-full self-center bg-spotifyGreen text-spotifyBlack data-[hover]:bg-spotifyBlack data-[hover]:text-spotifyGreen transition ease-in-out duration-500 px-4 py-2"
+            onClick={() => Login(code ?? "", setUserProfile)}
+          >
+            Login to Spotify
+          </Button>
+          <p className="text-spotifyText max-w-[512px]">
+            Please login to spotify so in order to fetch your listening data.
+            Nothing is saved or persisted in any way
+          </p>
+        </div>
+      )}
+      {userProfile && (
+        <div className="text-spotifyGreen">{`Welcome, ${userProfile.display_name}`}</div>
+      )}
     </div>
   );
 }
