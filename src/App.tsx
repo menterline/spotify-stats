d4ffa376cddc4076e1d1b2a3c217de3c8668654a
Matt Enterline <matthew.j.enterline@gmail.com>
2024-07-15 19:45:07 -0400

    Added react query

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./Components/LandingPage";
 import { Profile } from "./Components/Profile";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/spotify-stats" element={<LandingPage />} />
-          <Route path="/spotify-stats/profile" element={<Profile />} />h
+          <QueryClientProvider client={queryClient}>
+            <Route path="/spotify-stats" element={<LandingPage />} />
+            <Route path="/spotify-stats/profile" element={<Profile />} />h
+          </QueryClientProvider>
         </Routes>
       </BrowserRouter>
     </div>
