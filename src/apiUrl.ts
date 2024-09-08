6aa4afdd2231c1d2cb8a9a8177d9c70faa5e76ca
Matt Enterline <matthew.j.enterline@gmail.com>
2024-09-08 13:09:42 -0400

    hitting app service

--- src/apiUrl.ts
+++ src/apiUrl.ts
@@ -1,2 +1,3 @@
 //export const apiUrl = "http://localhost:8080";
-export const apiUrl = "http://20.81.146.174:8080";
+export const apiUrl =
+  "https://audioinsightsservice-ccaraubsgueteaap.centralus-01.azurewebsites.net";
