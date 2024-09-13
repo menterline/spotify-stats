79224e598e191c78b07c4fd8eb3808e77bcfb789
Matt Enterline <menterline.dev@gmail.com>
2024-09-12 22:13:22 -0400

    added button to go back to login page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,13 +1,20 @@
 import { useSpotifyLogin } from "../Hooks/hooks";
 import { Profile } from "../Components/Profile";
+import { SpotifyButton } from "../Components/SpotifyButton";
+import { useNavigate } from "react-router-dom";
 
 export function ProfilePage() {
   const params = new URLSearchParams(window.location.search);
   const code = params.get("code");
   const token = useSpotifyLogin(code ?? "");
-
+  const navigate = useNavigate();
   if (!token) {
-    return <div>Not logged in</div>;
+    return (
+      <div className="flex flex-col gap-16">
+        <div className="text-3xl text-spotifyText">Not logged in</div>
+        <SpotifyButton label="Login" onClick={() => navigate("/")} />
+      </div>
+    );
   }
   return <Profile token={token} />;
 }
